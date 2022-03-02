c549112b97e0c8c71f6db649f17e1f17dcc2c3a8
Igor <i_swat@mail.ru>
2022-03-02 12:06:57 +0300

    Добавлен и настроен UI компонент my-input

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,15 +1,13 @@
 <template>
 	<form class="form" @submit.prevent>
 		<h4>Создание поста</h4>
-		<input
+		<my-input
 			v-model="post.title"
-			class="input"
 			type="text"
 			placeholder="Название"
 		/>
-		<input
+		<my-input
 			v-model="post.body"
-			class="input"
 			type="text"
 			placeholder="Описание"
 		/>
@@ -45,13 +43,6 @@ export default {
 .form {
 	display: flex;
 	flex-direction: column;
-}
-
-.input {
-	width: 100%;
-	border: 1px solid teal;
-	padding: 10px 15px;
-	margin-top: 15px;
 }
 
 .btn {
