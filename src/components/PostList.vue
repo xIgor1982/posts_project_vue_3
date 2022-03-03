c9e13d775176361ec3d2b4e272f112fa0402cf67
Igor <i_swat@mail.ru>
2022-03-03 09:50:07 +0300

    Реализована анимация

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,16 @@
 <template>
 	<div v-if="posts.length > 0">
 		<h3>Список постов</h3>
-		<post-item
-			v-for="post in posts"
-			:key="post.id"
-			:post="post"
-			@remove="$emit('remove', post)"
-		/>
+		<transition-group name="user-list">
+			<post-item
+				v-for="post in posts"
+				:key="post.id"
+				:post="post"
+				@remove="$emit('remove', post)"
+			/>
+		</transition-group>
 	</div>
-	<div v-else style="color: red; text-align: center;">
+	<div v-else style="color: red; text-align: center">
 		<h2>Список постов пуст!</h2>
 	</div>
 </template>
@@ -26,4 +28,24 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.user-list-item {
+	display: inline-block;
+	margin-right: 10px;
+}
+.user-list-enter-active,
+.user-list-leave-active {
+	transition: all 0.4s ease;
+}
+.user-list-enter-from,
+.user-list-leave-to {
+	opacity: 0;
+	transform: translateX(130px);
+}
+.user-list-move {
+	transition: transform 0.4s ease;
+}
+.user-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
